46a3619cbc0ad61613c93f2d1c10864df1c602b1
KerniusSur <kernius.survila@gmail.com>
2022-10-10 11:02:51 +0300

    GRAF-20 add category CRUD

--- src/graffiti/graffiti.module.ts
+++ src/graffiti/graffiti.module.ts
@@ -1,7 +1,7 @@
 import { Module } from "@nestjs/common";
 import { GraffitiService } from "./graffiti.service";
 import { GraffitiController } from "./graffiti.controller";
-import { PrismaModule } from "src/prisma/prisma.module";
+import { PrismaModule } from "../prisma/prisma.module";
 
 @Module({
 	imports: [PrismaModule],
