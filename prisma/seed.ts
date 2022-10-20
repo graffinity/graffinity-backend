8ec11c7ed14eb2229c155746a64c7586439374f1
KerniusSur <74358398+KerniusSur@users.noreply.github.com>
2022-10-21 01:01:29 +0300

    Graf 20 implement nest js instead of express js (#24)
    * GRAF-20 graffiti CRUD
    
    * GRAF-20 ran lint
    
    * GRAF-20 dist fix + test setup
    
    * GRAF-20 add category CRUD
    
    * GRAF-20 update readme
    
    * GRAF-20 update readme v2
    
    * GRAF-20 dto + gitignore
    
    * GRAF-20 merge conflcit fix
    
    * GRAF-20 added swagger + deleted eslintrc.js + README.md update + faker and db seeding
    
    * GRAF-20 added seed script to package.json
    
    * GRAF-20 minor db seed change
    
    * GRAF-20 implementing swagger
    
    * GRAF-20 fix after merge
    
    * GRAF-20 ci fix + test fix + fix after merge

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,1 +1,29 @@
 import { PrismaClient } from "@prisma/client";
+import * as dotenv from "dotenv";
+
+import { faker } from "@faker-js/faker";
+
+const prisma = new PrismaClient();
+
+const graffitiTestData = (): any => ({
+	name: faker.internet.domainWord(),
+	description: faker.lorem.sentence(),
+	location: faker.address.city(),
+	createdAt: faker.date.past(),
+});
+
+async function main() {
+	const fakerRounds = 5;
+	dotenv.config();
+	console.log("Seeding...");
+	for (let i = 0; i < fakerRounds; i++) {
+		await prisma.graffiti.create({ data: graffitiTestData() });
+	}
+	console.log("Finished...");
+}
+
+main()
+	.catch((e) => console.error(e))
+	.finally(async () => {
+		await prisma.$disconnect();
+	});
