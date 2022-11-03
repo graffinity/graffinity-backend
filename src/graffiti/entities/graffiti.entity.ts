2e79114322149b0b9a4b3726ca1688abb48da039
Dominykas3000 <dominykas.svilpa@mif.stud.vu.lt>
2022-11-03 23:07:24 +0200

    ran lint

--- src/graffiti/entities/graffiti.entity.ts
+++ src/graffiti/entities/graffiti.entity.ts
@@ -1,6 +1,6 @@
-import { ApiProperty } from "@nestjs/swagger";
-import { CategoryToGraffiti, Graffiti } from "@prisma/client";
-import { CategoryEntity } from "../../category/entities/category.entity";
+import { ApiProperty } from '@nestjs/swagger';
+import { CategoryToGraffiti, Graffiti } from '@prisma/client';
+import { CategoryEntity } from '../../category/entities/category.entity';
 
 export class GraffitiEntity implements Graffiti {
 	@ApiProperty()
