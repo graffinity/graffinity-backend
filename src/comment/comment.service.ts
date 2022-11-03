4349fd93ed852ea8e5466127b08ebdaad8774b6b
Morta <morta.matelyte@mif.stud.vu.lt>
2022-11-03 18:26:43 +0200

    updated Comment entity crud

--- src/comment/comment.service.ts
+++ src/comment/comment.service.ts
@@ -1,46 +1,24 @@
 import { Injectable } from "@nestjs/common";
 import { PrismaService } from "../prisma/prisma.service";
-import { CreateCommentDto } from "./dto/create-comment.dto";
-import { UpdateCommentDto } from "./dto/update-comment.dto";
+import { CreateCommentDto } from "./dto/request/create-comment.dto";
 
 @Injectable()
 export class CommentService {
 	constructor(private prisma: PrismaService) {}
 
-	create(createCommentDto: CreateCommentDto) {
-		return this.prisma.comment.create({ data: createCommentDto });
+	async create(createCommentDto: CreateCommentDto) {
+		return await this.prisma.comment.create({data: createCommentDto});
 	}
 
-	findAll() {
-		return this.prisma.comment.findMany();
+	async findAll() {
+		return await this.prisma.comment.findMany();
 	}
 
-	findOne(id: number) {
-		return this.prisma.comment.findUnique({
+	async delete(id: number) {
+		return await this.prisma.comment.delete({
 			where: {
 				id: id,
 			},
 		});
-	}
-
-	update(id: number, updateCommentDto: UpdateCommentDto) {
-		return this.prisma.comment.update({
-			where: {
-				id: id,
-			},
-			data: updateCommentDto,
-		});
-	}
-
-	remove(id: number) {
-		return this.prisma.comment.delete({
-			where: {
-				id: id,
-			},
-		});
-	}
-
-	findAllFilteredBy() {
-		return this.prisma.comment.findMany({});
 	}
 }
