a3fa3c90aad80f00d5a598882d815288a54082df
ZivileNeb <zivile.neb@gmail.com>
2022-11-06 22:13:55 +0200

    Grafiitiphoto service ajustments

---
+++ src/graffitiphoto/dto/response/graffitiphoto.response.dto.ts
@@ -0,0 +1,23 @@
+import { ApiProperty } from "@nestjs/swagger";
+
+export class GraffitiPhotoResponseDto {
+    @ApiProperty({
+		description: "Id of graffiti photo",
+        example: 1,
+	})
+	id: number;
+
+	@ApiProperty({
+		description: "GraffitiId of graffiti photo",
+        example: 1,
+	})
+	graffitiId: number;
+
+	@ApiProperty({
+		description:
+			"The url of a graffiti photo",
+            example: "wwww.example.com",
+	})
+	url: string;
+
+}
