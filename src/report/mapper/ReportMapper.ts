e76bd6b3c0f8f7404115a7bc715491ec7ecd9b77
ZivileNeb <zivile.neb@gmail.com>
2022-11-20 15:33:12 +0200

    ReportUpdate

---
+++ src/report/mapper/ReportMapper.ts
@@ -0,0 +1,23 @@
+import { ReportResponseDto } from "../dto/response/report-response.dto";
+import { ReportEntity } from "../entities/report.entity";
+
+const ReportMapper = {
+	toEntity: () => {},
+
+	toResponse: (entity: ReportEntity) => {
+		let response: ReportResponseDto = {
+			id: entity.id,
+			graffitiId: entity.graffitiId,
+			userId: entity.userId,
+			reportReason: entity.reportReason,
+			status: entity.status,
+		};
+
+		return response;
+	},
+	toResponses: (entities: ReportEntity[]) => {
+		return entities.map((entity) => ReportMapper.toResponse(entity));
+	},
+};
+
+export default ReportMapper;
