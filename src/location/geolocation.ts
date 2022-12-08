f066970923f161a24e87175f4bfe012cd0041fd5
Morta <morta.matelyte@mif.stud.vu.lt>
2022-12-08 16:23:46 +0200

    location folder

---
+++ src/location/geolocation.ts
@@ -0,0 +1,8 @@
+import { geolocation } from '../package.json';
+var geolocation = require('geolocation');
+
+
+geolocation.getCurrentPosition(function (err, position) {
+	if (err) throw err;
+	console.log(position);
+});
