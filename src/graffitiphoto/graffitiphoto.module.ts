a33f221c0af7be49483182c58c2128e062fdc1d6
KerniusSur <74358398+KerniusSur@users.noreply.github.com>
2022-12-08 13:26:06 +0200

    GRAF-53 nest js file upload handling [WIP] (#63)
    * GRAF-53 cloudly deps
    
    * GRAF-31 error fix
    
    * GRAF-53 image uploading to amazon s3
    
    * minor changes
    
    * GRAF-53 added default import
    
    * update
    
    * GRAF-53 ran lint

--- src/graffitiphoto/graffitiphoto.module.ts
+++ src/graffitiphoto/graffitiphoto.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common';
 import { GraffitiPhotoService } from './graffitiphoto.service';
 import { GraffitiPhotoController } from './graffitiphoto.controller';
 import { PrismaModule } from '../prisma/prisma.module';
+import { S3Module } from '../s3/S3module';
+import S3Service from '../s3/S3service';
 
 @Module({
-	imports: [PrismaModule],
+	imports: [PrismaModule, S3Module],
 	controllers: [GraffitiPhotoController],
-	providers: [GraffitiPhotoService],
+	providers: [GraffitiPhotoService, S3Service],
 })
 export class GraffitiPhotoModule {}
