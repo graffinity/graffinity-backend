9b293d35d4e844f636fa4889ccb0783eb0748407
Morta <morta.matelyte@mif.stud.vu.lt>
2022-12-08 16:29:21 +0200

    ran lint

--- src/location/geolocation.ts
+++ src/location/geolocation.ts
@@ -1,7 +1,6 @@
 import { geolocation } from '../package.json';
 var geolocation = require('geolocation');
 
-
 geolocation.getCurrentPosition(function (err, position) {
 	if (err) throw err;
 	console.log(position);
