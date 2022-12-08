ac08e356a8abeba4cc39f93fd7b279ab77cd955b
Morta <morta.matelyte@mif.stud.vu.lt>
2022-12-08 16:33:41 +0200

    deleted location folder

--- src/location/geolocation.ts
+++
@@ -1,7 +0,0 @@
-import { geolocation } from '../package.json';
-var geolocation = require('geolocation');
-
-geolocation.getCurrentPosition(function (err, position) {
-	if (err) throw err;
-	console.log(position);
-});
