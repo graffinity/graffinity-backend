b899cf583c18199c17dded5ab8e5bf656dbd6d72
Morta <morta.matelyte@mif.stud.vu.lt>
2022-12-08 16:25:18 +0200

    Fixed all unit tests

--- src/graffitiphoto/graffitiphoto.controller.spec.ts
+++ src/graffitiphoto/graffitiphoto.controller.spec.ts
@@ -2,6 +2,7 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { PrismaService } from '../prisma/prisma.service';
 import { GraffitiPhotoController } from './graffitiphoto.controller';
 import { GraffitiPhotoService } from './graffitiphoto.service';
+import { S3Service } from '../s3/S3service';
 
 describe('GraffitiPhotoController', () => {
 	let controller: GraffitiPhotoController;
@@ -9,7 +10,7 @@ describe('GraffitiPhotoController', () => {
 	beforeEach(async () => {
 		const module: TestingModule = await Test.createTestingModule({
 			controllers: [GraffitiPhotoController],
-			providers: [GraffitiPhotoService, PrismaService],
+			providers: [S3Service, GraffitiPhotoService, PrismaService],
 		}).compile();
 
 		controller = module.get<GraffitiPhotoController>(GraffitiPhotoController);
