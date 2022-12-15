56151245c5daccde0ab85350e4f2182f4a3d7d32
KerniusSur <kernius.survila@gmail.com>
2022-12-15 11:44:03 +0200

    GRAF-38 test data factory / seed fix

--- prisma/data/util/TestDataFactory.ts
+++ prisma/data/util/TestDataFactory.ts
@@ -1,4 +1,10 @@
-import { Category, Graffiti, PrismaClient, User } from '@prisma/client';
+import {
+	Category,
+	CategoryToGraffiti,
+	Graffiti,
+	PrismaClient,
+	User,
+} from '@prisma/client';
 import argon2 from 'argon2';
 
 export class TestDataFactory {
@@ -12,6 +18,7 @@ export class TestDataFactory {
 		return TestDataFactory.instance;
 	}
 
+	// --------------------------------
 	// User test data
 	public getValidUser(): User {
 		let newUser: User = {
@@ -43,51 +50,48 @@ export class TestDataFactory {
 		return newUser;
 	}
 
+	// --------------------------------
 	// Category test data
 	public getValidCategory(): Category {
-		let newCategory = {
+		let newCategory: Category = {
 			id: 1,
 			name: 'Category',
-			description: 'Category description',
 		};
 
 		return newCategory;
 	}
 
 	public getListOfCategories(): Category[] {
-		let categories = [
+		let categories: Category[] = [
 			{
 				id: 1,
 				name: 'Category 1',
-				description: 'Category 1 description',
 			},
 			{
 				id: 2,
 				name: 'Category 2',
-				description: 'Category 2 description',
 			},
 			{
 				id: 3,
 				name: 'Category 3',
-				description: 'Category 3 description',
 			},
 		];
 
 		return categories;
 	}
 
+	// --------------------------------
 	// Graffiti test data
 	public getValidGraffiti(): Graffiti {
 		let newGraffiti = {
 			id: 1,
 			name: 'Graffiti name 1',
-			title: 'Graffiti title 1',
+
 			description: 'Graffiti description 1',
 			authorId: 1,
-			userId: 1,
-			categoryId: null,
+
 			createdAt: new Date(),
-			location: '54.687157,25.279652',
+			location: '54.687000,25.279000',
 			// Change location from string to LatLng
 			// latitude: 54.687157,
 			// longitude: 25.279652,
@@ -96,69 +100,67 @@ export class TestDataFactory {
 	}
 
 	public getValidGraffitiWithCategory(): Graffiti {
-		let newGraffiti = {
+		let newGraffiti: Graffiti = {
 			id: 1,
 			name: 'Graffiti name 1',
-			title: 'Graffiti title 1',
 			description: 'Graffiti description 1',
 			authorId: 1,
-			userId: 1,
-			categoryId: 1,
 			createdAt: new Date(),
-			location: '54.687157,25.279652',
+			location: '54.687000,25.279000',
 		};
 
 		return newGraffiti;
 	}
 
 	public getListOfGraffitis(): Graffiti[] {
-		let graffitiList = [
+		let graffitiList: Graffiti[] = [
 			{
 				id: 1,
 				name: 'Graffiti name 1',
-				title: 'Graffiti title 1',
 				description: 'Graffiti description 1',
 				authorId: 1,
-				userId: 1,
-				categoryId: 1,
 				createdAt: new Date(),
 				location: '54.660000,25.250000',
 			},
 			{
 				id: 2,
 				name: 'Graffiti name 2',
-				title: 'Graffiti title 2',
 				description: 'Graffiti description 2',
 				authorId: 1,
-				userId: 2,
-				categoryId: 2,
 				createdAt: new Date(),
 				location: '54.670000,25.260000',
 			},
 			{
 				id: 3,
 				name: 'Graffiti name 3',
-				title: 'Graffiti title 3',
 				description: 'Graffiti description 3',
 				authorId: 2,
-				userId: 1,
-				categoryId: 3,
 				createdAt: new Date(),
 				location: '54.680000,25.270000',
 			},
 			{
 				id: 4,
 				name: 'Graffiti name 4',
-				title: 'Graffiti title 4',
 				description: 'Graffiti description 4',
 				authorId: 2,
-				userId: 2,
-				categoryId: null,
 				createdAt: new Date(),
 				location: '54.690000,25.280000',
 			},
 		];
 
 		return graffitiList;
+	}
+
+	// --------------------------------
+	// Grafitti-To-Category test data
+
+	public getValidGraffitiToCategory(): CategoryToGraffiti {
+		let newCategoryToGraffiti: CategoryToGraffiti = {
+			id: 1,
+			categoryId: 1,
+			graffitiId: 1,
+		};
+
+		return newCategoryToGraffiti;
 	}
 }
