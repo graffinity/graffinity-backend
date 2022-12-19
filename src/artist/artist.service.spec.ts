50fc044258ff484d4dff877a957d1510cc3f9684
Morta <morta.matelyte@mif.stud.vu.lt>
2022-12-19 17:26:27 +0200

    Writing unit tests for artist.service.ts

--- src/artist/artist.service.spec.ts
+++ src/artist/artist.service.spec.ts
@@ -1,4 +1,5 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { Artist } from '@prisma/client';
 import { PrismaService } from '../prisma/prisma.service';
 import { ArtistService } from './artist.service';
 
@@ -16,4 +17,21 @@ describe('ArtistService', () => {
 	it('should be defined', () => {
 		expect(service).toBeDefined();
 	});
+
+	let artist1: Artist = {
+		id: 1,
+		name: 'Courtney',
+		surname: 'Tyler',
+		// graffitis: [],
+	};
+	it('should return all categories', async () => {
+		const artists = await service.findAll();
+
+		console.log('expected', artist1);
+		console.log('actual', artists[0]);
+
+		expect(artists[0]).toMatchObject(artist1);
+		expect(artists).toBeDefined();
+	});
 });
+
