c8ff512eb1116386ff0fd5f8254b3eb528ee1d62
Morta <morta.matelyte@mif.stud.vu.lt>
2022-12-30 18:51:26 +0200

    Created a create testing method for artist.service.ts, although it is not fully functioning.

--- src/artist/artist.service.spec.ts
+++ src/artist/artist.service.spec.ts
@@ -2,36 +2,56 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { Artist } from '@prisma/client';
 import { PrismaService } from '../prisma/prisma.service';
 import { ArtistService } from './artist.service';
+import { CreateArtistDto } from './dto/request/create-artist.dto';
 
 describe('ArtistService', () => {
 	let service: ArtistService;
 
 	beforeEach(async () => {
 		const module: TestingModule = await Test.createTestingModule({
-			providers: [PrismaService, ArtistService],
+			providers: [ArtistService],
 		}).compile();
 
 		service = module.get<ArtistService>(ArtistService);
 	});
+
+	afterEach(() => console.log('database teardown'));
+	afterEach(() => console.log('connection teardown'));
 
 	it('should be defined', () => {
 		expect(service).toBeDefined();
 	});
 
 	let artist1: Artist = {
-		id: 1,
+		id: 2,
 		name: 'Courtney',
 		surname: 'Tyler',
 		// graffitis: [],
 	};
-	it('should return all categories', async () => {
-		const artists = await service.findAll();
 
-		console.log('expected', artist1);
-		console.log('actual', artists[0]);
+	describe('Create method', () => {
+		it('should create and return an object of artist details', async () => {
+			let testArtist = {
+				name: 'Namenam',
+				surname: 'surnamesurnam',
+				graffitiIds: [3],
+			};
 
-		expect(artists[0]).toMatchObject(artist1);
-		expect(artists).toBeDefined();
+			const artist = await service.create(testArtist);
+			expect(artist).not.toBeNull();
+		});
+	});
+
+	describe('FindAll method', () => {
+		it('should return all artists', async () => {
+			const artists = await service.findAll();
+
+			console.log('expected', artist1);
+			console.log('actual', artists[0]);
+
+			//expect(artists[0]).toMatchObject(artist1);
+			expect(artists).toBeDefined();
+		});
 	});
 });
-
+	
