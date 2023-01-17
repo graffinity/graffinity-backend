1c9d019182e31a888e886d542c392f535595ec72
KerniusSur <kernius.survila@gmail.com>
2023-01-18 00:40:00 +0200

    GRAF-82 other misc updates

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-
-docker build -t $AWS_ECR_ENDPOINT/graffinity-backend:amd64 --platform linux/amd64 .
+docker build -t . 445007777844.dkr.ecr.eu-central-1.amazonaws.com/graffinity-backend
+# docker build -t 445007777844.dkr.ecr.eu-central-1.amazonaws.com/graffinity-backend:amd64 --platform linux/amd64 .
