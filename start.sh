d1270b513cca5e4e1f5fe160e0e4b06b6a99f084
KerniusSur <kernius.survila@gmail.com>
2023-01-19 01:13:54 +0200

    ci workflow dep fix

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 aws ecr get-login-password | docker login --username AWS --password-stdin 445007777844.dkr.ecr.eu-central-1.amazonaws.com
 docker compose pull
-docker compose up -d
+docker compose up -d --wait
