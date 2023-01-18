b6d2b11efe3ada6d37263e1eab862bc85f2fa3e4
KerniusSur <kernius.survila@gmail.com>
2023-01-19 00:16:56 +0200

    GRAF-82 build & push script fix

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-docker buildx build --load -f Dockerfile --platform=linux/amd64 . -t 445007777844.dkr.ecr.eu-central-1.amazonaws.com/graffinity-backend
+# Build the image to AWS ECR
+docker buildx build --load -f Dockerfile . -t 445007777844.dkr.ecr.eu-central-1.amazonaws.com/graffinity-backend
 # docker build -t 445007777844.dkr.ecr.eu-central-1.amazonaws.com/graffinity-backend:amd64 --platform linux/amd64 .
--- publish.sh
+++ publish.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
-# Login to AWS ECR
+# Login to AWS ECR ; Build and push the image to AWS ECR
 aws ecr get-login-password | docker login --username AWS --password-stdin 445007777844.dkr.ecr.eu-central-1.amazonaws.com
-
-# Build and push the image to AWS ECR
-docker buildx build --load -f Dockerfile --platform=linux/amd64 . -t 445007777844.dkr.ecr.eu-central-1.amazonaws.com/graffinity-backend
+docker buildx build --load -f Dockerfile . -t 445007777844.dkr.ecr.eu-central-1.amazonaws.com/graffinity-backend
 docker push 445007777844.dkr.ecr.eu-central-1.amazonaws.com/graffinity-backend
 
 # docker build -t 445007777844.dkr.ecr.eu-central-1.amazonaws.com/graffinity-backend:amd64 --platform linux/amd64 .
