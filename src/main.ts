79e6fd3e5e86513781881043076fda7909061bd5
KerniusSur <kernius.survila@gmail.com>
2023-01-19 15:41:28 +0200

    express session brought back & other minor changes

--- src/main.ts
+++ src/main.ts
@@ -3,39 +3,43 @@ import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import * as dotenv from 'dotenv';
 import 'dotenv/config';
-import session from 'cookie-session';
+// import session from 'cookie-session';
+import session from 'express-session';
 import passport from 'passport';
 import { AppModule } from './app.module';
 import { PrismaService } from './prisma/prisma.service';
 
 async function bootstrap() {
 	const app: INestApplication = await NestFactory.create(AppModule);
-	app.enableCors({
-		origin: [
-			'http://localhost:3000',
-			'http://graffinity.art',
-			'https://graffinity.art',
-			'https://graffinity-images.s3.eu-central-1.amazonaws.com',
-			'https://google.com',
-			'https://developers.google.com/',
-		],
-		credentials: true,
-		exposedHeaders: [
-			'Set-Cookie',
-			'Access-Control-Allow-Origin',
-			'Access-Control-Allow-Headers',
-		],
-		allowedHeaders: [
-			'Content-Type',
-			'Authorization',
-			'Access-Control-Allow-Origin',
-			'Access-Control-Allow-Headers',
-			'Access-Control-Allow-Methods',
-			'Access-Control-Allow-Credentials',
-		],
 
-		methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-	});
+	app.enableCors(); // Enable CORS for all routes
+
+	// app.enableCors({
+	// 	origin: [
+	// 		'http://localhost:3000',
+	// 		'http://graffinity.art',
+	// 		'https://graffinity.art',
+	// 		'https://graffinity-images.s3.eu-central-1.amazonaws.com',
+	// 		'https://google.com',
+	// 		'https://developers.google.com/',
+	// 	],
+	// 	credentials: true,
+	// 	exposedHeaders: [
+	// 		'Set-Cookie',
+	// 		'Access-Control-Allow-Origin',
+	// 		'Access-Control-Allow-Headers',
+	// 	],
+	// 	allowedHeaders: [
+	// 		'Content-Type',
+	// 		'Authorization',
+	// 		'Access-Control-Allow-Origin',
+	// 		'Access-Control-Allow-Headers',
+	// 		'Access-Control-Allow-Methods',
+	// 		'Access-Control-Allow-Credentials',
+	// 	],
+
+	// 	methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+	// });
 
 	app.useGlobalPipes(new ValidationPipe());
 	dotenv.config({ path: '.env' });
@@ -53,14 +57,32 @@ async function bootstrap() {
 
 	SwaggerModule.setup('api', app, document);
 
+	// ### Cookie session
+	// app.use(
+	// 	session({
+	// 		name: 'session',
+	// 		secret: 'keyboard',
+	// 		maxAge: 24 * 60 * 60 * 1000, // 24 hours
+	// 		sameSite: 'none',
+	// 	}),
+	// );
+	// ###
+
+	// ### Express session
 	app.use(
 		session({
-			name: 'session',
-			secret: 'keyboard',
-			maxAge: 24 * 60 * 60 * 1000, // 24 hours
-			sameSite: 'none',
+			secret: 'keyboard cat',
+			resave: false,
+			saveUninitialized: false,
+			cookie: {
+				secure: true,
+				sameSite: 'none',
+				maxAge: 24 * 60 * 60 * 1000, // 24 hours
+			},
 		}),
 	);
+	// ###
+
 	app.use(passport.initialize());
 	app.use(passport.session());
 
@@ -72,12 +94,6 @@ async function bootstrap() {
 
 	await app.listen(8080);
 
-	console.log(
-		`Application is running on: ${
-			(await app.getUrl()) === 'http://[::1]:8080'
-				? 'http://localhost:8080'
-				: app.getUrl()
-		}`,
-	);
+	console.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
